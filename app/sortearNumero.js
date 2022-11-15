b4bbb7266c47cb9ce90f0f2d5d6545119e29df20
Kaju-K <marcelokogut3@gmail.com>
2022-11-16 01:34:18 +0200

    first commit

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 0
+const maiorValor = 10
+
+const elementoMenorValor = document.getElementById("menor-valor")
+const elementoMaiorValor = document.getElementById("maior-valor")
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
+
+const numeroSecreto = gerarNumeroAleatorio(menorValor, maiorValor)
+
+function gerarNumeroAleatorio(min, max) {
+    return parseInt(Math.random()*(max - min + 1) + min)
+}
+
+console.log("Número secreto: " + numeroSecreto)
